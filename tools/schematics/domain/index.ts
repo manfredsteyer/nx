3b2e5bf0c875f73cfad2404dc4d23e34bd25ccf5
Manfred Steyer <manfred.steyer@gmx.net>
2019-06-25 00:38:03 +0200

    init

---
+++ tools/schematics/domain/index.ts
@@ -0,0 +1,42 @@
+import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
+
+export default function(schema: any): Rule {
+
+  return chain([
+    externalSchematic('@nrwl/workspace', 'lib', {
+      name: 'feature-create',
+      directory: schema.name,
+      routing: true,
+      lazy: true,
+      unitTestRunner: 'karma',
+      tags: `domain:${schema.name}, type:feature`
+    }),
+    externalSchematic('@nrwl/workspace', 'lib', {
+      name: 'feature-update',
+      directory: schema.name,
+      routing: true,
+      lazy: true,
+      unitTestRunner: 'karma',
+      tags: `domain:${schema.name}, type:feature`
+    }),
+    externalSchematic('@nrwl/workspace', 'lib', {
+      name: 'ui',
+      directory: schema.name,
+      unitTestRunner: 'karma',
+      tags: `domain:${schema.name}, type:ui`
+    }),
+    externalSchematic('@nrwl/workspace', 'lib', {
+      name: 'domain',
+      directory: schema.name,
+      unitTestRunner: 'karma',  
+      tags: `domain:${schema.name}, type:domain`    
+    }),
+    externalSchematic('@nrwl/workspace', 'lib', {
+      name: 'util',
+      directory: schema.name,
+      unitTestRunner: 'karma',      
+      tags: `domain:${schema.name}, type:util`
+    })
+
+  ]);
+}
